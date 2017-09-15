f7a9b3b4975139dd065896d25e983b47413e3a10
luokw <luokw@tpl.cntaiping.com>
2017-09-15 18:31:00 +0800

    dev

--- src/modules/fundament/common/basic/401.vue
+++ src/modules/fundament/common/basic/401.vue
@@ -1,11 +1,8 @@
 <template>
   <div class="error-tips">
-    <a href="javascript:;" class="logo">
-    </a>
-    <img src="imgs/error.png">
+    <img src="./imgs/error.png">
     <h2>没有权限访问页面～</h2>
     <p><router-link to="/">回到首页</router-link></p>
-
   </div>
 </template>
 <style lang="stylus">
--- src/modules/fundament/common/basic/404.vue
+++ src/modules/fundament/common/basic/404.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="error-tips">
-    <a href="javascript:;" class="logo">
-    </a>
+
     <img src="./imgs/error.png">
     <h2>咦～页面不见了～</h2>
     <p>请核对您输入的页面地址是否正确哦~</p>
